3ef28b3b06d914f30535713739ca058b8ce072b5
saad <saad@koubeissi.com>
2017-01-14 20:40:44 +0000

    Added form validation

--- js/main.js
+++ js/main.js
@@ -7,6 +7,10 @@ function saveBookmark(e) {
     var siteName = document.getElementById('siteName').value;
     var siteUrl = document.getElementById('siteUrl').value;
 
+    if (!validateForm(siteName, siteUrl)) {
+        return false;
+    }
+
     // Object ready to submit to localStorage
     var bookmark = {
         name: siteName,
@@ -81,4 +85,24 @@ function fetchBookmarks() {
                                       '</h3>' +
                                       '</div>';
     }
+}
+
+// Form validation
+
+function validateForm(siteName, siteUrl) {
+    if (!siteName || !siteUrl) {
+        alert('Please fill in the form');
+        return false;
+    }
+
+    // Regular expression to format a URL
+    var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+    var regex = new RegExp(expression);
+
+    if (!siteUrl.match(regex)) {
+        alert('Please use a valid URL');
+        return false;
+    }
+
+    return true;
 }
